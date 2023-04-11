e891735c40c73b8c01a4ee4e18354b58405f9a1d
Javok23 <130308615+Javok23@users.noreply.github.com>
2023-04-11 19:41:26 +0200

    Add files via upload

---
+++ src/main/java/jenkinsmaven/mavenejer5/Mavenejer5Application.java
@@ -0,0 +1,13 @@
+package jenkinsmaven.mavenejer5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mavenejer5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mavenejer5Application.class, args);
+	}
+
+}
---
+++ src/main/java/jenkinsmaven/mavenejer5/ServletInitializer.java
@@ -0,0 +1,13 @@
+package jenkinsmaven.mavenejer5;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Mavenejer5Application.class);
+	}
+
+}
